f6c5fb031a654306503feb28160d98edd528966d
GauravJangra9988 <gjangra9988@gmail.com>
2025-09-30 23:24:55 +0530

    updated frontend url

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
     app.use((req, res, next) => {
       res.header(
         "Access-Control-Allow-Origin",
-        "https://rag-frontend-umber.vercel.app/"
+        "https://rag-frontend-umber.vercel.app"
       );
       res.header("Access-Control-Allow-Credentials", "true");
       res.header(
